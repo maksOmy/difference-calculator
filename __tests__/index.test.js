523c40068f36b9c425e48cfc87c12d0ab1608c6a
maksOmy <antipov.maks00@mail.ru>
2020-09-09 15:34:54 +0400

    refactoring

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -20,27 +20,24 @@ const iniFile2 = getFixturePath('before.ini');
 
 test('stylishFormatDiff', () => {
   const expectedResult = reader('__fixtures__/stylishResult.txt');
-  const expectedResultForIni = reader('__fixtures__/stylishResultForIni.txt');
 
   expect(gendiff(jsonFile1, jsonFile2)).toEqual(expectedResult);  
   expect(gendiff(ymlFile1, ymlFile2)).toEqual(expectedResult); 
-  expect(gendiff(iniFile1, iniFile2)).toEqual(expectedResultForIni);
+  expect(gendiff(iniFile1, iniFile2)).toEqual(expectedResult);
 });
 
 test('plainFormatDiff', () => {
   const expectedResult = reader('__fixtures__/plainResult.txt');
-  const expectedResultForIni = reader('__fixtures__/plainResultForIni.txt');
 
   expect(gendiff(jsonFile1, jsonFile2, 'plain')).toEqual(expectedResult);
   expect(gendiff(ymlFile1, ymlFile2, 'plain')).toEqual(expectedResult); 
-  expect(gendiff(iniFile1, iniFile2, 'plain')).toEqual(expectedResultForIni);
+  expect(gendiff(iniFile1, iniFile2, 'plain')).toEqual(expectedResult);
 });
 
 test('jsonFormatDiff', () => {
   const expectedResult = reader('__fixtures__/jsonResult.txt');
-  const expectedResultForIni = reader('__fixtures__/jsonResultForIni.txt');
 
   expect(gendiff(jsonFile1, jsonFile2, 'json')).toEqual(expectedResult);
   expect(gendiff(ymlFile1, ymlFile2, 'json')).toEqual(expectedResult);
-  expect(gendiff(iniFile1, iniFile2, 'json')).toEqual(expectedResultForIni);
+  expect(gendiff(iniFile1, iniFile2, 'json')).toEqual(expectedResult);
 });
